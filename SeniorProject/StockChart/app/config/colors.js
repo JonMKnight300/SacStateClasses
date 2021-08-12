9f0e0faa1cbdd5140c5ec9a8e78537df3ec997f1
Jon Mercurio Knight <84875131+JonMKnight300@users.noreply.github.com>
2021-08-12 11:13:00 -0700

    Add files via upload

--- SeniorProject/StockChart/app/config/colors.js
+++ SeniorProject/StockChart/app/config/colors.js
@@ -1,1 +1,13 @@
+export default {
+    primary: "#fc5c65",
+    secondary: "#4ecdc4",
+    black: "#000",
+    white: "#fff",
+    medium: "#6e6969",
+    light: "#d3d3d3",
+    danger: "#ff5252",
+    dark: "#0c0c0c",
+    positive: "#72c372",
+    negative: 	"#ee2400",
 
+}
